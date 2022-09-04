ba2964e0bda1b377e88dacb9901c21b8905d53e1
vikas pundir <vikaspundiruser@gmail.com>
2022-09-05 02:52:25 +0530

    redux

--- Redux-project-udemy/src/components/Counter.js
+++ Redux-project-udemy/src/components/Counter.js
@@ -5,6 +5,7 @@ const Counter = () => {
 
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
+  const show =  useSelector(state => state.showCounter);
 
   const incrementHandler = () => {
     dispatch ({ type: 'increment'})
@@ -15,14 +16,22 @@ const Counter = () => {
     dispatch ({ type: 'decrement'})
   }
 
-  const toggleCounterHandler = () => {};
+
+  const increaseHandler = () =>{
+    dispatch ({ type: 'increase' , amount : 5})
+  }
+
+  const toggleCounterHandler = () => {
+    dispatch({type:'toggle'})
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <button onClick={incrementHandler}>Increment</button>
       <button onClick={decrementHandler}>Decrement</button>
+      <button onClick={increaseHandler}>Increase by 5</button>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
