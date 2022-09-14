74461c73592c3c77a1049ff308dd35a9cf0d57c7
vikas pundir <vikaspundiruser@gmail.com>
2022-09-15 04:17:32 +0530

    react-router

--- React-Router-Project/src/pages/QuoteDetail.js
+++ React-Router-Project/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams , Route } from 'react-router-dom'
+import { useParams , Route , Link } from 'react-router-dom'
 import HighlightedQuote from '../components/quotes/HighlightedQuote'
 
 import Comments from '../components/comments/Comments'
@@ -28,6 +28,12 @@ function QuoteDetail() {
   return (
     <div>
     <HighlightedQuote text = {quote.text} author = {quote.author} />
+    <Route path={`/quotes/${params.quoteId}`} exact>
+    <div className='centered'>
+        <Link className='btn--flat' to= {`/quotes/${params.quoteId}/comments`}>Load Comments</Link> 
+     </div>
+    </Route>
+    
     <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments/>
     </Route>
